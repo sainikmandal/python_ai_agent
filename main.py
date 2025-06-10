e5830baffdc309276853fbfbd0ff2e90f9d60e8f
sainikm <isainikkhan@gmail.com>
2025-06-11 00:46:46 +0530

    add user promt in main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "Hey! write your prompt here"')
+        print('Example: python main.py "How do I forget the trauma of React?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
+
